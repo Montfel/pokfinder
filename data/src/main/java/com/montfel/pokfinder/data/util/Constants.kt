fd044b4d19fe8538be3d65b75f34dfab25b33882
Felipe <felipemonteirose@gmail.com>
2024-07-03 12:26:03 -0300

    Melhora performance da lista

--- data/src/main/java/com/montfel/pokfinder/data/util/Constants.kt
+++ data/src/main/java/com/montfel/pokfinder/data/util/Constants.kt
@@ -4,5 +4,5 @@ object Constants {
     const val POKFINDER_DATABASE = "pokfinder_database"
     const val POKEMON_HOME_TABLE = "pokemon_home_table"
     const val POKEMON_HOME_REMOTE_KEYS_TABLE = "pokemon_home_remote_keys_table"
-    const val ITEMS_PER_PAGE = 10
+    const val ITEMS_PER_PAGE = 50
 }
