31013b95510be3b5705e435a361ed08c9021ef5e
Felipe <felipemonteirose@gmail.com>
2024-07-11 13:45:18 -0300

    atualiza version code

--- buildSrc/src/main/kotlin/ProjectConfig.kt
+++ buildSrc/src/main/kotlin/ProjectConfig.kt
@@ -3,6 +3,6 @@ object ProjectConfig {
     const val compileSdkVersion = 34
     const val targetSdkVersion = 34
 
-    const val versionCode = 8
-    const val versionName = "1.0.7"
+    const val versionCode = 9
+    const val versionName = "1.1.0"
 }
