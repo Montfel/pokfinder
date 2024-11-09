dedbe6c0d6d42c03a4635347eaf9bd58ca47c666
Felipe <felipemonteirose@gmail.com>
2024-11-09 13:07:46 -0300

    Sobe versão

--- buildSrc/src/main/kotlin/ProjectConfig.kt
+++ buildSrc/src/main/kotlin/ProjectConfig.kt
@@ -3,6 +3,6 @@ object ProjectConfig {
     const val compileSdkVersion = 34
     const val targetSdkVersion = 34
 
-    const val versionCode = 10
-    const val versionName = "1.2.0"
+    const val versionCode = 11
+    const val versionName = "1.2.1"
 }
