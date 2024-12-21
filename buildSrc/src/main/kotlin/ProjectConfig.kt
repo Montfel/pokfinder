cfa629bb6179cb5b2ba992f8adfe4d97cd6155c8
Felipe <felipemonteirose@gmail.com>
2024-12-21 19:03:18 -0300

    sobe para android 15

--- buildSrc/src/main/kotlin/ProjectConfig.kt
+++ buildSrc/src/main/kotlin/ProjectConfig.kt
@@ -1,7 +1,7 @@
 object ProjectConfig {
     const val minSdkVersion = 24
-    const val compileSdkVersion = 34
-    const val targetSdkVersion = 34
+    const val compileSdkVersion = 35
+    const val targetSdkVersion = 35
 
     const val versionCode = 11
     const val versionName = "1.2.1"
