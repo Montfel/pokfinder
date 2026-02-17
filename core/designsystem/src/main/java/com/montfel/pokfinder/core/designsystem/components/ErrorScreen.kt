56e63eae58c839ff8bbc584e49483404906a4420
Felipe <felipemonteirose@gmail.com>
2026-02-17 14:09:18 -0300

    error screen preview

--- core/designsystem/src/main/java/com/montfel/pokfinder/core/designsystem/components/ErrorScreen.kt
+++ core/designsystem/src/main/java/com/montfel/pokfinder/core/designsystem/components/ErrorScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.montfel.pokfinder.core.designsystem.R
 import com.montfel.pokfinder.core.designsystem.theme.PokfinderTheme
@@ -48,5 +49,13 @@ fun ErrorScreen(
                 )
             }
         }
+    }
+}
+
+@Preview
+@Composable
+private fun ErrorScreenPreview() {
+    PokfinderTheme {
+        ErrorScreen(onClick = {})
     }
 }
